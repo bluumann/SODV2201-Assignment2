178e37edf9122de618258ea4f774bb69e7ae6dfc
Peter Kosa <p.kosa089@mybvc.ca>
2022-11-27 12:38:22 -0700

    Add create/delete course feature

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,58 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 const port = 5000;
 
+// Import courses array from data.js
+const { courses } = require('../backend/database/data.js');
+
+// API: default route
 app.get('/', (req, res) => {
   res.send('BVC Course Registration Backend');
+});
+
+// API: retrieve all courses
+app.get('/api/courses', (req, res) => {
+  res.send(courses);
+});
+
+// API: retrieve a course by id
+app.get('/api/courses/:courseCode', (req, res) => {
+  const course = courses.find(c => c.courseCode === req.params.courseCode);
+  if (!course)
+    return res
+      .status(404)
+      .send('The course with the given "course code" was not found.');
+  res.send(course);
+});
+
+// API: create new course
+app.post('/api/courses', (req, res) => {
+  const course = {
+    courseCode: req.body.courseCode,
+    courseName: req.body.courseName,
+    courseTerm: req.body.courseTerm,
+    courseStartDate: req.body.courseStartDate,
+    courseEndDate: req.body.courseEndDate,
+    courseFees: req.body.courseFees,
+    courseDescription: req.body.courseDescription,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+// API: delete a course
+app.delete('/api/courses/:courseCode', (req, res) => {
+  const course = courses.find(c => c.courseCode === req.params.courseCode);
+  if (!course)
+    return res
+      .status(404)
+      .send('The course with the given "course code" was not found.');
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
 });
 
 app.listen(port, () => {
